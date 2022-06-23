5e5246670d6f0a4e6c6b7caabdd5dd304e835951
Babsdrey29 <Babsdrey29@users.noreply.github.com>
2022-06-23 22:18:09 +0100

    first task javascript-zuri

---
+++ intro.js
@@ -0,0 +1,9 @@
+function personalDetails(){
+    let fullName = 'Babalola Samuel D ' + '\n';
+    let myHeight = '160cm' + '\n';
+    let myCountry = 'Nigeria';
+
+   alert(fullName + myHeight + myCountry);
+
+}
+personalDetails();
